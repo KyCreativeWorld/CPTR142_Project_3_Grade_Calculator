6a2227736d2ae2776201eb393f4e1c52241a526e
Ky Kendall <kycreativeworld@gmail.com>
2026-03-02 15:20:49 -0800

    Test commit

---
+++ StudentClass.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <vector>
+#include <string>
+
+class StudentClass {
+    public:
+        StudentClass() {}
+
+    private:
+        std::vector<std::string> assignments;
+        std::vector<int> assignmentWeights;
+
+
+};
