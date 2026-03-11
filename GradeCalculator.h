bd46be7e006d532baeb806a481eb6c2b0591e815
Spencer Shipowick <spencershipowick38@gmail.com>
2026-03-11 16:13:55 -0700

    adins .h file

--- GradeCalculator.h
+++ GradeCalculator.h
@@ -1,19 +1,18 @@
-#ifndef GRADECALCULATOR_H
-#define GRADECALCULATOR_H
+// GradeCalculator.h
+#pragma once
+#include <string>
+#include "Gradebook.h"
+#include "StudentClass.h"
 
-class GradeCalculator {
-private:
-    double assignmentAvg;
-    double examAvg;
-    double assignmentWeight;
-    double examWeight;
+// from chat becuse i am not familuir with maps and hw to work with them
+double computeWeightedAverage(const std::map<std::string, StudentClass::ContentInfo>& content);
 
-public:
-    GradeCalculator(double aAvg, double eAvg, double aWeight, double eWeight);
+// Determines the minimum final exam grade needed to hit the desired score.
+//
+// Formula used from google:   desiredGrade = (assignmentWeightTotal * assignmentAvg)+ (examWeightTotal * examAvg) + (finalExamWeight * finalGrade)
+//   Solving for finalGrade: finalGrade = (desiredGrade - assignmentContribution - examContribution)/ finalExamWeight
+double calcRequiredFinalExamGrade(StudentClass& cls, double desiredGrade);
 
-    double requiredExamScore(double targetFinal);
-    double requiredAssignmentScore(double targetFinal);
-    double currentFinalGrade();
-};
-
-#endif
+// finds the class, runs the calculation, prints results.
+void processGradeGoal(Gradebook& gb, int quarterNum,
+                      const std::string& className, double desiredGrade);
