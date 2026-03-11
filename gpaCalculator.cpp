9f136c1e3568569a40173c0d31a1010c3f065621
Spencer Shipowick <spencershipowick38@gmail.com>
2026-03-11 16:06:52 -0700

    updated to do each quarter and use calculated grades in other file

--- gpaCalculator.cpp
+++ gpaCalculator.cpp
@@ -6,20 +6,6 @@
 #include "StudentClass.h"
 using namespace std;
 
-//Grade values
-double letterToPoints(string letter) {
-    if (letter == "A") return 4.0;
-    if (letter == "A-") return 3.7;
-    if (letter == "B+") return 3.3;
-    if (letter == "B") return 3.0;
-    if (letter == "B-") return 2.7;
-    if (letter == "C+") return 2.3;
-    if (letter == "C") return 2.0;
-    if (letter == "C-") return 1.7;
-    if (letter == "D+") return 1.3;
-    if (letter == "D") return 1.0;
-    return 0.0; //F or unknown
-}
 
 double calculateTotalGPA(Gradebook& gb) {
     double totalWeightedPoints = 0.0;
@@ -36,7 +22,7 @@ double calculateTotalGPA(Gradebook& gb) {
             //Only calculate for courses with grades
             if (course.getLetterGrade() != "NR" && course.getLetterGrade() != "") {
                 int credits = course.getClassCredits();
-                double gradePoints = letterToPoints(course.getLetterGrade());
+                double gradePoints = course.getNumberGrade();
                 
                 //Add to specific quarter bucket
                 quarterPoints += (gradePoints * credits);
